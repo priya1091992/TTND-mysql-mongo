a448100f5a802a3b1a4853a1dfef7f850192a3bb
Priya Choudhary <priya@tothenew.com>
2016-04-11 01:52:44 +0530

    mongoose schema creation

---
+++ mongoose_collection.js
@@ -0,0 +1,53 @@
+/**
+ * Created by priya on 10/4/16.
+ */
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopping');
+var connection = mongoose.connection;
+
+connection.on('error', function () {
+  console.log('###### Error ####');
+});
+connection.on('connected', function () {
+  console.log('###### Connected ####');
+});
+
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    UserId:{type:Number, required:true},
+    Name:{ type: String, required: true},
+    Address:{
+      building:{type:String}
+    },
+    PhoneNumber: {type: Number},
+    email:[{type: String, required: true}]}
+);
+
+var userModel = mongoose.model('User', User);
+
+var Product=new Schema({
+  ProductID:{type:Number},
+  Description:{type:String},
+  Handling:{type:String}
+})
+var product=mongoose.model('Product',Product);
+
+var Order=new Schema({
+OrderID:{type:Number},
+  OrderDetails:[
+    {
+      Productid:{ type: Number},
+      quantity:{type:Number}
+    }
+  ],
+  UserID:{type:Number}
+})
+var orderItem=mongoose.model('Order',Order);
+
+module.exports={
+  userModel:userModel,
+  product:product,
+  orderItem:orderItem
+};
